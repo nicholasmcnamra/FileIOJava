aaa49a0d42ffd85e814abfc1728084896529a3d4
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-09 14:24:00 -0500

    wrote what I think would work but it seems like the file I'm passing in is not found. need to fix this still

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -10,9 +10,25 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             // Print out a running total of all the
             // values in the input file.
+            int sum = 0;
+            try {
+                Scanner fileIn = new Scanner(new File("input.txt"));
+
+                while (fileIn.hasNext()) {
+
+                    String lineIn = fileIn.nextLine();
+                    String[] input = lineIn.split(",");
+                    for (int i = 0; i < input.length; i ++) {
+                        sum += Integer.parseInt(input[i]);
+                    }
+                    System.out.println(sum);
+                }
+            }
+            catch (IOException e) {
+                System.out.println("File not found.");
+            }
         }
 }
